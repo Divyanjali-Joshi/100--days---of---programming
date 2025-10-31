ff9cb3981eca1ae333532233b029e202abfd98a5
Divyanjali-Joshi <sanyujoshi2114@gmail.com>
2025-10-31 18:51:27 +0530

    Add files via upload

---
+++ day 20(b).c
@@ -0,0 +1,23 @@
+ #include <stdio.h>
+
+int main() {
+    long long binary, complement = 0, place = 1;
+    int digit;
+    printf("Enter a binary number: ");
+    scanf("%lld", &binary);
+
+    while (binary > 0) {
+        digit = binary % 10;    
+        if (digit == 0){
+            complement += 1 * place;
+        }else{
+            complement += 0 * place;
+    }
+        place =place* 10;
+        binary=binary/ 10;
+    }
+
+    printf("1's Complement = %lld\n", complement);
+
+    return 0;
+}
