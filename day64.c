3b62f55004355da5dcee530819f60455a3b7f33b
Divyanjali-Joshi <sanyujoshi2114@gmail.com>
2025-11-06 22:42:32 +0530

    Update day64.c

--- day64.c
+++ day64.c
@@ -1,4 +1,24 @@
- #include <stdio.h>
+Q114: Write a program to take a string s as input. The task is to find the length of the longest substring without repeating characters. Print the length as output.
+
+/*
+Sample Test Cases:
+Input 1:
+s = "abcabcbb"
+Output 1:
+3
+
+Input 2:
+s = "bbbbb"
+Output 2:
+1
+
+Input 3:
+s = "pwwkew"
+Output 3:
+3
+
+*/
+#include <stdio.h>
 #include <string.h>
 
 int longestUniqueSubstring(char *s) {
@@ -34,3 +54,4 @@ int main() {
 
     return 0;
 }
+
