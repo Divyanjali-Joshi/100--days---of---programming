a4308510a09856440c1e96215618a0766755890b
Divyanjali-Joshi <sanyujoshi2114@gmail.com>
2025-11-06 12:44:47 +0530

    Update natural.c

--- natural.c
+++ natural.c
@@ -1,3 +1,5 @@
+//Write a program to find and display the sum of the first n natural numbers.
+
 #include <stdio.h>
 int main()
 {
@@ -8,3 +10,4 @@ int main()
     printf("sum=%d \n",sum);
     return 0;
 }
+
