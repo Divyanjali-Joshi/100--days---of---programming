c944ca0176e63b2d3d6dea1b8bb1771889f5cfff
Divyanjali-Joshi <sanyujoshi2114@gmail.com>
2025-11-06 21:43:38 +0530

    Update library.c

--- library.c
+++ library.c
@@ -1,4 +1,9 @@
- #include <stdio.h>
+/*Write a program to calculate library fine based on late days as follows: 
+First 5 days late: ₹2/day 
+Next 5 days late: ₹4/day 
+Next 20 days days late: ₹6/day 
+More than 30 days: Membership Cancelled.
+#include <stdio.h>*/
 int main()
 {
     int a=0;
@@ -20,4 +25,5 @@ int main()
         printf("membership cancelled");
     }
     return 0;
+
 }
