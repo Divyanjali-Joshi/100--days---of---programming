370d95ff8175fb841e289e53451d78ccd93dd33d
Divyanjali-Joshi <sanyujoshi2114@gmail.com>
2025-11-06 21:44:14 +0530

    Refactor bill.c with comments for clarity
    Added comments to clarify the electricity bill calculation logic.

--- bill.c
+++ bill.c
@@ -1,4 +1,10 @@
- #include <stdio.h>
+/*Write a program to calculate electricity bill based on units consumed with these rates: 
+First 100 units at ₹5/unit 
+Next 100 units at ₹7/unit 
+Next 100 units at ₹10/unit 
+Above at ₹12/unit*/
+
+#include <stdio.h>
 int main()
 {
     int a=0;
@@ -22,4 +28,5 @@ int main()
 
     }
     return 0;
+
 }
