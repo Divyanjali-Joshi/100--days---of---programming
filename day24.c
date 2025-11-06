a7bf45f73bcf8a567781456a9c1c80a009befdca
Divyanjali-Joshi <sanyujoshi2114@gmail.com>
2025-11-06 22:03:40 +0530

    Update day24.c

---
+++ day24.c
@@ -0,0 +1,24 @@
+/*Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+*/
+ 
+
+
+#include <stdio.h>
+int main(){
+    int n= 5;
+
+    for(int i=0; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            printf("*");
+        }
+        printf(" ");
+    }
+    return 0;
+}
+
+
