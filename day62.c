5ebcf6bc629d1cc2c6b8400ae236483b82c3a715
Divyanjali-Joshi <sanyujoshi2114@gmail.com>
2025-11-06 22:41:51 +0530

    Update day62.c

--- day62.c
+++ day62.c
@@ -1,4 +1,25 @@
- #include <stdio.h>
+Q112: Write a program to take an integer array arr as input. The task is to find the maximum sum of any contiguous subarray using Kadane's algorithm. Print the maximum sum as output. If all elements are negative, print the largest (least negative) element.
+
+/*
+Sample Test Cases:
+Input 1:
+arr[] = [2, 3, -8, 7, -1, 2, 3]
+Output 1:
+11
+
+Input 2:
+arr[] = [-2, -4]
+Output 2:
+-2
+
+Input 3:
+arr[] = [5, 4, 1, 7, 8]
+Output 3:
+25
+
+*/
+
+#include <stdio.h>
 
 int maxSubarraySum(int arr[], int n) {
     int max_so_far = arr[0];
@@ -34,3 +55,4 @@ int main() {
 
     return 0;
 }
+
